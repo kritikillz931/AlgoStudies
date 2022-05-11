8883ae6e8e0b97e0a489c76ac5d40b3306394537
Justin <justinadams758@gmail.com>
2022-05-11 16:59:58 -0600

    solved

--- testing2.js
+++ testing2.js
@@ -77,3 +77,22 @@ function check(arrayA, valueX) {
     }
   }
 //   check(arrayA, valueX)
+
+// ------------------------------S O L V E D------------------------------ \\
+
+//Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+// Name Opposites Attract
+const flower1 = [1]
+const flower2 = [4]
+function lovefunc(flower1, flower2){
+    console.log(flower1, flower2)
+  if(flower1 % 2 == 0 && flower2 % 2 != 0 || flower1 % 2 != 0 && flower2 % 2 == 0) {
+    return true
+  } else {
+    return false
+  }
+}
+// lovefunc(flower1, flower2)
+
+// ------------------------------S O L V E D------------------------------ \\
